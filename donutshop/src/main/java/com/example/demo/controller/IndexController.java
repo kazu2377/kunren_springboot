ad2a80002eeb4b21f081d5ee6201f1376269d4db
kazu2377 <jtths474@hotmail.co.jp>
2023-02-10 19:37:24 +0900

    ok

---
+++ donutshop/src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//@RestController
+//@SpringBootApplication
+@Controller
+public class IndexController {
+
+//    @GetMapping("/")
+//    String home() {
+//        return "Hello World!";
+//    }
+
+    @GetMapping("/")
+	public String getIndex() {
+		return "index.html";
+	}
+
+}
